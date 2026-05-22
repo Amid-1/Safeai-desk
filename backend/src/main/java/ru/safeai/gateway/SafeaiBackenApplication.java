2697ed0a8408c3e02bdca428d908e6d257a0dcd5
Amid-1 <Amid.1spbtj@gmail.com>
2026-05-22 22:46:51 +0300

    add spring boot backend skeleton

---
+++ backend/src/main/java/ru/safeai/gateway/SafeaiBackenApplication.java
@@ -0,0 +1,13 @@
+package ru.safeai.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeaiBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeaiBackenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/safeai/gateway/SafeaiBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safeai.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeaiBackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
