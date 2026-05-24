43aa404e6ddb8f09a72d7e16fbc005d09cfb259e
Amid-1 <Amid.1spbtj@gmail.com>
2026-05-24 21:22:27 +0300

    configure backend database and local infrastructure

--- backend/src/main/java/ru/safeai/gateway/SafeaiBackendApplication.java
+++ backend/src/main/java/ru/safeai/gateway/SafeaiBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SafeaiBackenApplication {
+public class SafeaiBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SafeaiBackenApplication.class, args);
+		SpringApplication.run(SafeaiBackendApplication.class, args);
 	}
 
 }
